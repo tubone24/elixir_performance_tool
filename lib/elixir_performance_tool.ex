21e5e0c21e373076cd4919fdc2778e6ba678f17b
tubone24 <tubo.yyyuuu@gmail.com>
2020-01-13 21:40:02 +0900

    ebookhomebrew

--- lib/elixir_performance_tool.ex
+++ lib/elixir_performance_tool.ex
@@ -16,15 +16,40 @@ defmodule ElixirPerformanceTool do
   def send_requests_parallel(process_num,count) do
     time_total = Enum.map(1..process_num, &Task.async(fn ->
       &1
-      send_request("https://ebook-homebrew.herokuapp.com")
+      send_requests("https://ebook-homebrew.herokuapp.com/")
     end))
                  |> Enum.map(fn(task) -> Task.await(task,1000_000) end)
                  |> Enum.reduce(0, fn x,total -> total + x end)
     IO.inspect "#{count}, average_time: #{time_total / process_num / 1000} ms, time_total: #{time_total / 1000} ms"
   end
 
-  def send_request(url) do
-    {time, _} = :timer.tc(fn -> HTTPoison.get!(url,[],[{:timeout, 1000000000}]) end)
+  def send_requests(url) do
+    time_1 = send_status(url)
+    {time_2, upload_id} = send_fileupload(url)
+    time_3 = send_convert_pdf(url, upload_id)
+    time = time_1 + time_2 + time_3
+    time
+  end
+
+  def send_status(url) do
+    status_url = url <> "status"
+    {time, _} = :timer.tc(fn -> HTTPoison.get!(status_url,[],[{:timeout, 1000000000}]) end)
+    time
+  end
+
+  def send_fileupload(url) do
+    upload_url = url <> "data/upload"
+    {_, encoded_png} = File.read("encoded_png.txt")
+    payload = Poison.encode!(%{"contentType" => "image/png", "images" => [encoded_png]})
+    {time, ret} = :timer.tc(fn -> HTTPoison.post!(upload_url, payload, [],[{:timeout, 1000000000}]) end)
+    upload_id = Poison.decode!(ret.body)["upload_id"]
+    {time, upload_id}
+  end
+
+  def send_convert_pdf(url, upload_id) do
+    convert_url = url <> "convert/pdf"
+    payload = Poison.encode!(%{"contentType" => "image/png", "uploadId" => upload_id})
+    {time, _} = :timer.tc(fn -> HTTPoison.post!(convert_url, payload, [],[{:timeout, 1000000000}]) end)
     time
   end
 end
